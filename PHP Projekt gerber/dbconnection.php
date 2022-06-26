f8b0cc959c75ceeaf137bd80fb811fcc44241f71
grumpyavner <104833159+grumpyavner@users.noreply.github.com>
2022-06-26 06:57:04 +0200

    Update dbconnection.php
    unnötigen Code entfernt

--- PHP Projekt gerber/dbconnection.php
+++ PHP Projekt gerber/dbconnection.php
@@ -7,25 +7,4 @@ $verbindung = mysqli_connect($server, $user, $pass, $db);
 if (mysqli_connect_errno()) {
 	echo "<p>Es ist ein Verbindungsfehler aufgetreten.</p>";
 } 
-
-// $sql = "SELECT * FROM bestellung";
-//                 $ergebnis = mysqli_query($verbindung, $sql);
-//                 if($ergebnis)	{
-//                     echo "<p>Die SQL-Anweisung war erfolgreich...</p>";
-//                     $anzahl = mysqli_num_rows($ergebnis);
-//                     echo "<p>In der Tabelle befinden sich $anzahl Datensätze:</p>";
-//                     while ($zeile = mysqli_fetch_array($ergebnis)){
-//                         echo $zeile["ID"] .", "
-//                             .$zeile["Vorname"] .", "
-//                             .$zeile["Nachname"] .", "
-//                             .$zeile["Ort"] .", "
-//                             .$zeile["Sorte"] .", "
-//                             .$zeile["Menge"] ." kg<br>";
-//                     } 
-//                     else {
-//                         echo "SQL-Fehler!<br>SQL meldet: " .mysqli_error($verbindung);   
-//                     }
-//                     mysqli_close($verbindung);
-//                     echo "<p>Die Verbindung mit dem Server wurde beendet.";
-//                 }
 ?>
